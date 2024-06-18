2bec0a7353aba4eef90e056e22ee8dad3c2b1436
Harsimran <singh.harsimran1308@gmail.com>
2024-06-17 20:00:48 -0400

    Configuring repo

---
+++ OrderPlacement/src/test/java/com/example/OrderPlacement/OrderPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderPlacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ OrderShipment/src/main/java/com/example/OrderShipment/OrderShipmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderShipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderShipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderShipmentApplication.class, args);
+	}
+
+}
