b256de750558f8f691ff47e5483ad16f2c6d6518
Harsimran <singh.harsimran1308@gmail.com>
2024-07-05 18:08:19 -0400

    Connected Producer to queue

--- OrderPlacement/src/main/java/com/example/OrderPlacement/Controller/ProcessOrder.java
+++ OrderPlacement/src/main/java/com/example/OrderPlacement/Controller/ProcessOrder.java
@@ -6,10 +6,13 @@ import com.example.OrderPlacement.Service.OrderShipping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/processOrder")
+@EnableJms
 public class ProcessOrder {
 
     private OrderShipping orderShipping = new OrderShipping();
@@ -17,16 +20,20 @@ public class ProcessOrder {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
     @PostMapping("/placeOrder")
     public ResponseEntity<String> placeOrder(@RequestBody OrderDetails orderDetail) {
         order = orderShipping.saveOrder(orderDetail);
         System.out.println(order);
-//        try {
+        try {
             orderRepository.save(order);
+            jmsTemplate.convertAndSend("DEV.QUEUE.1","Order Successfull");
             return new ResponseEntity<>("Order Placed", HttpStatus.CREATED);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>("Order Unsuccessful", HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Order Unsuccessful", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
---
+++ OrderShipment/src/main/java/com/example/OrderShipment/Controller/EventListener.java
@@ -0,0 +1,13 @@
+package com.example.OrderShipment.Controller;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventListener {
+
+    @JmsListener(destination = "DEV.QUEUE.1")
+    public void listen(String message) {
+        System.out.println("Ship Order");
+    }
+}
